6170666bb80886c7cf2170891bfbf00de93cfcbe
UmangSinghal0504 <umangsinghal2022256@gmail.com>
2025-07-10 03:50:52 -0700

    Correction

--- server/server.js
+++ server/server.js
@@ -16,13 +16,14 @@ app.use('/api/transactions', transactionRoute);
 
 const port = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === 'production'){
+/*if(process.env.NODE_ENV === 'production'){
     app.use('/', express.static('client/build'))
 
     app.use('*',(req,res) => {
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
     })
 }
+    */
 
 app.get('/', (req,res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Server running on port ${port}!`));
