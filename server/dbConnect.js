bb8ccdb8567abcf14461d3ea4d4e3855847f5409
UmangSinghal0504 <umangsinghal2022256@gmail.com>
2025-07-10 02:27:32 -0700

    Correction

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const connectionString = 'mongodb+srv://umangsinghal20222056:A0987654321z@cluster0.04stbym.mongodb.net/your-database-name'; // Add your DB name
+const connectionString = 'mongodb+srv://umangsinghal20222056:A0987654321z@cluster0.04stbym.mongodb.net/your-database-name'; 
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
